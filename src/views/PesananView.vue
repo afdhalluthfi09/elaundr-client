5e299cb6e1ccf6d2c005dbbd4e96b4244f801063
afdhalluthfi09 <afdhal.luthfi@student.amikom.ac.id>
2023-06-26 11:53:50 +0700

    perubahan strutuktur

--- src/views/PesananView.vue
+++ src/views/PesananView.vue
@@ -1,20 +1,60 @@
 <template>
-  <PesananComponent/>
+  <div class="container-pesanan">
+    <!-- header -->
+    <HeaderComponent/>
+    <!-- status -->
+    <StatusComponent/>
+    <!-- main -->
+    <div class="main">
+      <div class="message-title">Pesan Aktif</div>
+      <div class="contain-message">
+        <ul>
+          <li v-for="(item, index) in items" :key="index">
+            <div class="card-body collapse-button" @click="toggleItem(index)">
+              <div class="contain-body">
+                <img src="../assets/gambar/icon/icon-cuci.svg" alt="">
+                <div class="display-flex-column">
+                    <div class="detail-status">Pesanan No.{{ item.noPesana }}</div>
+                    <div class="detail-status">{{ item.status }}</div>
+                </div>
+              </div>
+            </div>
+            <div v-if="item.isExpanded">
+              {{ item.description }}
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import PesananComponent from '../components/Pesanan.vue'
+import HeaderComponent from '../components/Header.vue'
+import StatusComponent from '../components/Status.vue'
+
 export default {
   name: "PesananViewComponent",
   created() {},
   data() {
-    return {};
+    return {
+      items: [
+        { name: "Item 1", description: "Pakaian Anda Sudah Selesai Di Proses Sebentar Lagi Akan Di antar Ke Lokasi Anda",noPesana: "0002142", status: "selesai",isExpanded: false },
+        { name: "Item 2", description: "Pakaian Anda Sedang Selesai Di Proses Sebentar Lagi Akan Di antar Ke Lokasi Anda",noPesana: "0002142", status: "belum selesai",isExpanded: false },
+        { name: "Item 3", description: "Pakaian Anda Sedang Di Jemput Sebentar Lagi Ke Lokasi Anda",noPesana: "0002142", status: "jemput",isExpanded: false },
+      ],
+    };
   },
   components:{
-    PesananComponent
+    HeaderComponent,
+    StatusComponent
   },
   props: {},
-  methods: {},
+  methods: {
+    toggleItem(index) {
+      this.items[index].isExpanded = !this.items[index].isExpanded;
+    },
+  },
 };
 </script>
 
--- src/views/SettingView.vue
+++ src/views/SettingView.vue
@@ -1,16 +1,42 @@
 <template>
-  <SettingComponent/>
+  <div class="container-setting">
+    <!-- header -->
+    <HeaderComponent/>
+    <!-- bar status -->
+    <StatusComponent/>
+    <!-- main -->
+    <div class="main">
+            <div class="main-title">
+                <p>Layanan Kami</p>
+            </div>
+            <div class="contain-main">
+                <div id="openModalBtn" class="square-icon">
+                    <img src="../assets/gambar/icon/profile.svg" alt="">
+                    <p>Profil</p>
+                </div>
+                <div class="square-icon">
+                    <img src="../assets/gambar/icon/password.svg" alt="">
+                    <p>Password</p>
+                </div>
+                <div class="square-icon">
+                    <img src="../assets/gambar/icon/help.svg" alt="">
+                    <p>Bantuan</p>
+                </div>
+            </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import SettingComponent from '../components/Setting.vue'
+import HeaderComponent from '../components/Header.vue'
+import StatusComponent from '../components/Status.vue'
 export default {
   name: "SettingViewComponent",
   created() {},
   data() {
     return {};
   },
-  components:{SettingComponent},
+  components:{HeaderComponent,StatusComponent},
   props: {},
   methods: {},
 };
