29dff341b44b22b4293e03ce8f50b175c17360cc
afdhalluthfi09 <afdhal.luthfi@student.amikom.ac.id>
2023-07-11 15:01:55 +0700

    perubahan

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,14 @@
 <template>
   <router-view></router-view>
 </template>
-
 <script>
-// step 1: import sdk firebase dari situs resminya
-import { initializeApp } from "firebase/app";
-import { getMessaging, getToken,onMessage } from "firebase/messaging";
-// step 2: konfigurasi firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyC3exjydwPaTCwTIqX6zOxnmDNaB-JEcII",
-  authDomain: "e-laundry-client.firebaseapp.com",
-  projectId: "e-laundry-client",
-  storageBucket: "e-laundry-client.appspot.com",
-  messagingSenderId: "547200687551",
-  appId: "1:547200687551:web:5c04a31dc406b3af67550a"
-};
-
-// step 3: implement 
-const app = initializeApp(firebaseConfig);
-
-app
-// step 5: buat file dengan nama 'firebase-messaging-sw.js' letakan di folder public
-// step 6: import onMessage dari package "firebase/messaging";
-const messaging = getMessaging();
-onMessage(messaging, (payload) => {
-  console.log('Message received. ', payload);
-  // ...
-});
-
-// step 4: pemerikasaan token dari project firebase dengan memanggil getToken()
-getToken(messaging, { vapidKey: 'BJXNaNETK3BmmygbDb0SZ6gHts_XFtXj1FyRkUSiH4NvHIITjGw359OL2IMEMIV7Ckm0TjoGAoruAcsi9Xw2ybM' }).then((currentToken) => {
-  if (currentToken) {
-    console.log('token :', currentToken);
-    // Send the token to your server and update the UI if necessary
-    // ...
-  } else {
-    // Show permission request UI
-    console.log('No registration token available. Request permission to generate one.');
-    // ...
+import {app,messaging} from './firebaseConfig';
+export default {
+  created(){
+    app,
+    messaging
   }
-}).catch((err) => {
-  console.log('An error occurred while retrieving token. ', err);
-  // ...
-});
+}
 </script>
 <style lang="scss">
 @import "@/assets/index.scss";
